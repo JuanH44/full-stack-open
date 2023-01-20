23e1e687d4bf3c6d9f982c0424ea5cc75a04bbcb
JuanHdez <juanh7522@gmail.com>
2023-01-19 23:50:18 -0600

    Part2. 2.1-2.5: Curse information done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Course = ({course}) => (
+	<>
+			<Header course={course} />
+			<Content course={course} />
+			<Total course={course} />
+	</>
+);
+
+const Header = ({course:{name}}) => <h2>{name}</h2>;
+
+const Content = ({course:{parts}}) => {
+	return (
+		<div>
+			{parts.map(part => 
+				<Part key={part.id} part={part} />
+			)}
+		</div>
+	);
+};
+
+const Part = ({part:{name, exercises}}) => <p>{name} {exercises}</p>;
+
+const Total = ({course: {parts}}) => {
+	const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+	return (
+		<div>
+			<p><strong>total of {total} exercises</strong></p>
+		</div>
+	);
+};
+
+export default Course;
